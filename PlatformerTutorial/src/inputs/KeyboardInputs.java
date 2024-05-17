6a3c82a1576419bf8e2f4d9edb25fb0bf620aed1
ZeroZerge <163840769+ZeroZerge@users.noreply.github.com>
2024-05-17 22:07:50 +0800

    Initial commit

---
+++ PlatformerTutorial/src/inputs/KeyboardInputs.java
@@ -0,0 +1,47 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import gamestates.Gamestate;
+import main.GamePanel;
+
+public class KeyboardInputs implements KeyListener {
+
+	private GamePanel gamePanel;
+
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (Gamestate.state) {
+		case MENU : gamePanel.getGame().getMenu().keyReleased(e);
+		break;
+		case PLAYING : gamePanel.getGame().getPlaying().keyReleased(e);
+		break;
+		case CREDITS : gamePanel.getGame().getCredits().keyReleased(e);
+		break;
+		}
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (Gamestate.state) {
+		case MENU : gamePanel.getGame().getMenu().keyPressed(e);
+		break;
+		case PLAYING : gamePanel.getGame().getPlaying().keyPressed(e);
+		break;
+		case OPTIONS : gamePanel.getGame().getGameOptions().keyPressed(e);
+		break;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// Not In Use
+	}
+}
---
+++ PlatformerTutorial/src/inputs/MouseInputs.java
@@ -0,0 +1,86 @@
+package inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import gamestates.Gamestate;
+import main.GamePanel;
+
+public class MouseInputs implements MouseListener, MouseMotionListener {
+
+	private GamePanel gamePanel;
+
+	public MouseInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		switch (Gamestate.state) {
+		case PLAYING : gamePanel.getGame().getPlaying().mouseDragged(e);
+		break;
+		case OPTIONS : gamePanel.getGame().getGameOptions().mouseDragged(e);
+		break;
+		}
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		switch (Gamestate.state) {
+		case MENU : gamePanel.getGame().getMenu().mouseMoved(e);
+		break;
+		case PLAYING : gamePanel.getGame().getPlaying().mouseMoved(e);
+		break;
+		case OPTIONS : gamePanel.getGame().getGameOptions().mouseMoved(e);
+		break;
+		}
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		switch (Gamestate.state) {
+		case PLAYING : gamePanel.getGame().getPlaying().mouseClicked(e);
+		}
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void mousePressed(MouseEvent e) {
+		switch (Gamestate.state) {
+		case MENU : gamePanel.getGame().getMenu().mousePressed(e);
+		break;
+		case PLAYING : gamePanel.getGame().getPlaying().mousePressed(e);
+		break;
+		case OPTIONS : gamePanel.getGame().getGameOptions().mousePressed(e);
+		break;
+		}
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		switch (Gamestate.state) {
+		case MENU : gamePanel.getGame().getMenu().mouseReleased(e);
+		break;
+		case PLAYING : gamePanel.getGame().getPlaying().mouseReleased(e);
+		break;
+		case OPTIONS : gamePanel.getGame().getGameOptions().mouseReleased(e);
+		break;
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// Not In use
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// Not In use
+	}
+
+}
